5f772b519870b0b5fcc8d797df92ce4db0be6183
bhargavbhatiya <bhatiyabhargav.bb@gmail.com>
2024-04-12 18:01:02 +0530

    Set Auth cookie for tld to resolve error

--- apps/api/src/services/auth/auth.helper.ts
+++ apps/api/src/services/auth/auth.helper.ts
@@ -9,16 +9,31 @@ import config from 'config';
 export const setTokenCookies = ({
   ctx,
   accessToken,
-}: { ctx: AppKoaContext, accessToken: string }) => {
+}: {
+  ctx: AppKoaContext;
+  accessToken: string;
+}) => {
   const parsedUrl = url.parse(config.WEB_URL);
 
   if (!parsedUrl.hostname) {
     return;
   }
-
   const parsed = psl.parse(parsedUrl.hostname) as psl.ParsedDomain;
-  const cookiesDomain = parsed.domain || undefined;
+  /* Browser will not allow to set cookies for the domain that is not a subdomain of the current domain. so instead of setting cookies for the whole domain, we set it for the current subdomain only. */
 
+  // const cookiesDomain = parsed.domain || undefined;
+  const cookiesDomain = parsed.tld || undefined;
+
+  // console.log("parsedUrl.hostname", parsedUrl.hostname); parsedUrl.hostname js-vacancy-test-task-web-sg3f.onrender.com
+  // console.log("parsed", parsed);
+  //  Output: parsed {
+  //    input: 'js-vacancy-test-task-web-sg3f.onrender.com',
+  //    tld: 'onrender.com',
+  //    sld: 'js-vacancy-test-task-web-sg3f',
+  //    domain: 'js-vacancy-test-task-web-sg3f.onrender.com',
+  //    subdomain: null,
+  //    listed: true
+  //  }
   ctx.cookies.set(COOKIES.ACCESS_TOKEN, accessToken, {
     httpOnly: true,
     domain: cookiesDomain,
